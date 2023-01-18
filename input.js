fbd463ae9450fb365f173322337a887191bbb4d7
Sam <sammynarduzzi@gmail.com>
2023-01-18 15:21:19 -0800

    changes

--- input.js
+++ input.js
@@ -1,31 +1,31 @@
-const setupInput = function () {
+let connection;
+
+  const handleUserInput = function (data) {
+    if (data === "w") {
+        connection.write("Move: up")
+    } else if (data === "a") {
+        connection.write("Move: left")
+    } else if (data === "s"){
+        connection.write("Move: down")
+    } else if (data === "d") {
+        connection.write("Move: right")
+    // } else if (data === "q") {
+    //   connection.write("q");
+    //   process.exit();
+    }
+  };
+    
+const setupInput = function (conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
-  
-  const handleUserInput = function (data) {
-    if (data === "w") {
-      conn.on("connect", () => {
-        conn.write("Move: up")
-      })
-    } else if (data === "a") {
-      cconn.on("connect", () => {
-        conn.write("Move: left")
-      })
-    } else if (data === "s"){
-      conn.on("connect", () => {
-        conn.write("Move: down")
-      })
-    } else if (data === "d") {
-      conn.on("connect", () => {
-        conn.write("Move: right")
-      })
-    }
-    stdin.on("data", handleUserInput);
-  }
+  stdin.on("data", handleUserInput)
   return stdin;
 };
+
+
 
 module.exports = {
   setupInput,
